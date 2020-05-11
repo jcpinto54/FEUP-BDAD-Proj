ca68620551aa937c5ee634affe496cec883db677
Alexandre Abreu <44318137+a3brx@users.noreply.github.com>
2020-05-11 17:58:35 +0100

    added hard1

--- sql/queries.sql
+++ sql/queries.sql
@@ -14,6 +14,25 @@ SELECT isbn, max(countries) FROM (
 	) WHERE idUser = idperson GROUP BY isbn
 );
 
+-- hard1: qual a diferença da nota média das compras entre utilizadores de Portugal e de fora?
+SELECT PT.avg - EX.avg FROM (
+    SELECT AVG(rate) AS avg FROM SellerEvaluation NATURAL JOIN (
+        SELECT evaluation AS id FROM Selling NATURAL JOIN (
+            SELECT idperson AS iduser, code FROM User NATURAL JOIN (
+                SELECT localitycode, code FROM Locality JOIN Country WHERE namecountry = code
+            ) WHERE code = 'PT'
+        )
+    )
+) AS PT,
+    (SELECT AVG(rate) AS avg FROM SellerEvaluation NATURAL JOIN (
+        SELECT evaluation AS id FROM Selling NATURAL JOIN (
+            SELECT idperson AS iduser, code FROM User NATURAL JOIN (
+                SELECT localitycode, code FROM Locality JOIN Country WHERE namecountry = code
+            ) WHERE code <> 'PT'
+        )
+    )
+) AS EX;
+
 -- avg for the easy question 3 
 select avg(q.price), b.name, p.isbn 
 from (  select price, isbn 
