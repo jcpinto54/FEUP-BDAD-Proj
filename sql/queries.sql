f118285e1e6123a93fb501124221db0ba6695a79
João Pinto <joaocastropintoz@gmail.com>
2020-05-12 16:45:33 +0100

    hard2 done

--- sql/queries.sql
+++ sql/queries.sql
@@ -109,19 +109,9 @@ SELECT PT.avg - EX.avg FROM (
 ) AS EX;
 
 -- hard2: qual o idioma mais vendido em cada país e sua percentagem das vendas? (falta fazer percentagem!!)
-select CountryN, LangN, nrSales
-from (
-  select DISTINCT Country.code CC, Language.code LC, Count(Selling.idpublication) nrSales, Country.name CountryN, Language.name LangN
-  from Selling Join Publication on Selling.idPublication = Publication.id
-      join User on Publication.iduser = User.idperson
-      natural join Locality
-      join Country on Locality.nameCountry = Country.code
-      join Book on Publication.isbn = Book.ISBN
-      natural JOIN BookLanguage
-      jOIN Language on Language.code = BookLanguage.codelanguage   
-      Group by Country.code, Language.name)  T
-where nrSales = (select max(nrSales) from 
-                 (select DISTINCT Country.code CC, Language.code LC, Count(Selling.idpublication) nrSales
+drop view if exists countryLanguageSales;
+create temp view if not exists countryLanguageSales as 
+select DISTINCT Country.code CC, Language.code LC, Count(Selling.idpublication) nrSales, Country.name CountryN, Language.name LangN
                   from Selling Join Publication on Selling.idPublication = Publication.id
                       join User on Publication.iduser = User.idperson
                       natural join Locality
@@ -129,15 +119,23 @@ where nrSales = (select max(nrSales) from
                       join Book on Publication.isbn = Book.ISBN
                       natural JOIN BookLanguage
                       jOIN Language on Language.code = BookLanguage.codelanguage   
-                      Group by Country.code, Language.name) TT
-                 where T.CC = TT.CC);
---vendas totais por pais
-select DISTINCT Country.name, Count(Selling.idpublication) totalSales
-from Selling Join Publication on Selling.idPublication = Publication.id
-	join User on Publication.iduser = User.idperson
-    natural join Locality
-    join Country on Locality.nameCountry = Country.code
-group by Country.code;
+                      Group by Country.code, Language.name;
+
+select CountryN Country, LangN Language,  (100 * nrSales/totalSales) percentage from
+  (select CC, LC, nrSales, CountryN, LangN
+  from countryLanguageSales T
+  where nrSales = (select max(nrSales) 
+                   from countryLanguageSales TT
+                   where T.CC = TT.CC))
+  NATURAL join
+  --vendas totais por pais
+  (select DISTINCT Country.name as CountryN, Count(Selling.idpublication) totalSales
+  from Selling Join Publication on Selling.idPublication = Publication.id
+      join User on Publication.iduser = User.idperson
+      natural join Locality
+      join Country on Locality.nameCountry = Country.code
+  group by Country.code
+);
 
 -- hard3: qual o id dos usuários que tiveram todos os seus livros vendidos?
 -- Quantidade de publicações de cada utilizador
