f201497c28a01b065924d6f779c457c5aae5e2f1
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-05-12 20:43:58 +0100

    Necessário modificar povoar
    No atual povoar a query acima retorna algo vazio. Para ser retornado alguma coisa, devem ser adicionadas linhas como as abaixo: 
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 9378052939488);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(30,37,2,20);
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 9378052939488);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(31,35,2,20);
    INSERT INTO SellerEvaluation(comment,rate) VALUES ('Se eu não fosse teimoso, tinha pago o meu salário por um livro!',3);
    
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(32,35,2,20);
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 9378052939488);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(33,33,2,20);
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 8292939857557);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(34,34,2,20);
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 8292939857557);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(35,31,2,20);
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 8292939857557);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(36,22,2,20);
    
    INSERT INTO Publication(description, price, idUser, ISBN) VALUES('Comprei este livro há 2 anos. Gostei muito de lê-lo. Agora está na prateleira à procura de um novo dono! É muito bom!', 895.27, 5, 8292939857557);
    INSERT INTO Selling(idPublication, idUser,idPayment, evaluation) VALUES(37,21,2,20);

--- sql/int10.sql
+++ sql/int10.sql
@@ -1,5 +1,31 @@
 .mode columns
 .headers on
 .nullvalue NULL
+-- Hard 3
+-- Quais os utilizadores que compraram um livro em comum e qual o livro? 
+-- Pares de usuários na ordem inversa que partilham o mesmo livro não devem aparecer novamente. Nomeadamente o caso abaixo: 
+-- user1 | user2 | isbn | 
+-- us1   | us2   | 1    | 
+-- us2   | us1   | 1    | 
+-- Por outro lado, se dois usuarios compraram o mesmo livro mais de uma vez eles irão aparecer repetidamente na lista. 
 
--- hard3: quais os utilizadores que compraram um livro em comum?
+CREATE TABLE IF NOT EXISTS pairUser (user1, user2, isbn); 
+CREATE TABLE IF NOT EXISTS auxiliar (user1, user2, isbn); 
+
+CREATE TRIGGER IF NOT EXISTS t
+AFTER INSERT ON pairUser
+FOR EACH ROW  
+WHEN NOT EXISTS (SELECT * FROM pairuser WHERE (user1 = new.user2 AND user2 = new.user1 AND isbn = new.isbn) ) 
+BEGIN
+INSERT INTO auxiliar(user1, user2, isbn) VALUES(new.user1, new.user2, new.isbn);  
+END; 
+
+INSERT INTO pairUser (user1, user2, isbn) 
+SELECT s1.user AS user1, s2.user AS user2, s1.isbn
+FROM idUserBook s1, idUserBook s2
+WHERE s1.isbn = s2.isbn AND s2.user <> s1.user; 
+
+SELECT * FROM auxiliar; 
+
+DROP TABLE pairuser; 
+DROP TABLE auxiliar; 
