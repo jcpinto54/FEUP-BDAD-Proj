f98b15d0071995fe995acde3e0f176ea4f70da25
Jumaruba <juliane.marubayashi@gmail.com>
2020-05-15 21:10:35 +0100

    fixing query 10, teacher didn't like the idea of triggers on a query

--- sql/int10.sql
+++ sql/int10.sql
@@ -9,23 +9,11 @@
 -- us2   | us1   | 1    | 
 -- Por outro lado, se dois usuarios compraram o mesmo livro mais de uma vez eles irão aparecer repetidamente na lista. 
 
-CREATE TABLE IF NOT EXISTS pairUser (user1, user2, isbn); 
-CREATE TABLE IF NOT EXISTS auxiliar (user1, user2, isbn); 
+create view if not exists idUserBook as 
+select p.isbn as isbn, s.idUser as user 
+from selling s, publication p 
+where p.id = s.idPublication; 
 
-CREATE TRIGGER IF NOT EXISTS t
-AFTER INSERT ON pairUser
-FOR EACH ROW  
-WHEN NOT EXISTS (SELECT * FROM pairuser WHERE (user1 = new.user2 AND user2 = new.user1 AND isbn = new.isbn) ) 
-BEGIN
-INSERT INTO auxiliar(user1, user2, isbn) VALUES(new.user1, new.user2, new.isbn);  
-END; 
-
-INSERT INTO pairUser (user1, user2, isbn) 
-SELECT s1.user AS user1, s2.user AS user2, s1.isbn
-FROM idUserBook s1, idUserBook s2
-WHERE s1.isbn = s2.isbn AND s2.user <> s1.user; 
-
-SELECT * FROM auxiliar; 
-
-DROP TABLE pairuser; 
-DROP TABLE auxiliar; 
+select s1.user as user1, s2.user as user2, s1.isbn 
+from idUserBook s1, idUserBook s2 
+where s1.isbn = s2.isbn and s2.user < s1.user; 
