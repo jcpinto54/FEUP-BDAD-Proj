95c030f9206559426c03f3984247b7779d8eb386
João Pinto <joaocastropintoz@gmail.com>
2020-05-19 14:00:54 +0100

    working on 2 trigger

---
+++ sql/gatilho2_adiciona.sql
@@ -0,0 +1,9 @@
+Create Trigger deleteUser
+After Delete On User
+For Each Row
+Begin
+Delete from Publication where (
+	idUser = Old.idPerson 
+	AND
+    Publication.id not in (select idPublication from Selling));
+End;
