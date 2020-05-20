db045fa0e13efa9e1c279119ebccf7feda2fee78
Jumaruba <juliane.marubayashi@gmail.com>
2020-05-20 15:34:22 +0100

    gatilho3

--- sql/triggers/gatilho3_adiciona.sql
+++ sql/triggers/gatilho3_adiciona.sql
@@ -1,15 +1,17 @@
 create trigger gatilho3 
 before delete on User 
 for each row 
-when exists (
+when ( exists (
     select idUser 
-    from publication, User 
-    where publication.idUser = User.idPerson and old.idPerson = User.idPerson ) 
+    from publication, User
+    where publication.idUser = User.idPerson and old.idPerson = User.idPerson and 
+    exists (select * from selling where selling.idPublication = publication.id))
     OR
-    exists(
+    --person has purchased something
+    exists( 
         select idUser 
-        from selling, user 
-        where selling.idUser = user.idPerson and old.idPerson = User.idPerson) 
+        from selling, user
+        where selling.idUser = user.idPerson and old.idPerson = User.idPerson) )
     
 begin 
     select raise(ignore);
--- sql/triggers/gatilho3_verifica.sql
+++ sql/triggers/gatilho3_verifica.sql
@@ -1,35 +1,23 @@
---Case that the user didn't performed any publication neither purchase 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (190, 934330216, "aividinha@gmail.com", 44268836171088, "199, Rua dos bobos, Narnia", 000, 1000); 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (189, 934330217, "aividamon@gmail.com", 44268836171089, "200, Rua dos bobos, Narnia", 001, 1001);
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (188, 934330215, "aividamonamia@gmail.com", 44268836171090, "201, Rua dos bobos, Narnia", 002, 1002);
---Users that did a purchase 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (187, 934330213, "aividinho@gmail.com", 44268836174087, "129, Rua dos bobos, Narnia", 003, 1004); 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (186, 934330214, "aividamoo@gmail.com", 44268836171086, "220, Rua dos bobos, Narnia", 004, 1005);
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (185, 934330212, "aividamonamio@gmail.com", 44268836171099, "221, Rua dos bobos, Narnia", 005, 1006);
---Users that did a publication 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (184, 934333213, "aividinhr@gmail.com", 44268826171087, "229, Rua dos bobos, Narnia", 006, 1007); 
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (183, 934330814, "aividamor@gmail.com", 44268826171086, "420, Rua dos bobos, Narnia", 007, 1008);
-insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (182, 934300212, "aividamonamir@gmail.com", 44368836171099, "321, Rua dos bobos, Narnia", 008, 1306);
+
+insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (189, 934330216, "TEST1@gmail.com",  41278833120087, "rua dos bobos, numero zero", 123, 6449); 
+insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (190, 934332316, "TEST3@gmail.com",  41378833123087, "rua dos bobos, numero zero", 123, 6449); 
+insert into User(idPerson, contact, email, account, address, streetCode, localityCode) values (188, 934320216, "TEST2@gmail.com",  41278883120087, "rua dos bobos, numero zero", 123, 6449); 
+insert into Publication(description, price, idUser, ISBN) VALUES('TEST2', 231.0, 188, 4870295404161);
+insert into Selling(idPublication, idUser,idPayment, evaluation) VALUES(31, 189, 1, 5);
 
 
-insert into publication(description, price , idUser,ISBN) values ("Livro em bom estado, com pouco uso! Enredo muito interessante. Obra-prima!", 300, 184, 4870295404161);  --idPublication = 38 
-insert into publication(description, price, idUser,ISBN) values  ("Livro em bom estado, com pouco uso! Enredo muito interessante. Obra-prima!", 300, 183, 4870295404161);  --idPublication = 39 
-insert into publication(description, price , idUser,ISBN) values ("Livro em bom estado, com pouco uso! Enredo muito interessante. Obra-prima!", 300, 182, 4870295404161);  --idPublication = 40 
+select * from User; 
 
-insert into selling(idPublication, idUser, idPayment, evaluation) values (38, 187, 1, 3); 
-insert into selling(idPublication, idUser, idPayment, evaluation) values (39, 186, 1, 3);  
-insert into selling(idPublication, idUser, idPayment, evaluation) values (40, 185, 1, 3);  
+delete from User where idPerson = 189; 
 
+select * from User; 
 
---Try to delete users that do not have neither a publication nor purchase   -- SUCCESS EXPECTED 
-delete from User 
-where idPerson = 188 or idPerson = 189 or idPerson = 190; 
--- Try to delete users that did a selling                                   -- FAIL EXPECTED 
-delete from User 
-where idPerson = 187 or idPerson = 186 or idPerson = 185; 
--- Try to delete useras that did a publication                              --FAIL EXPECTED 
-delete from User 
-where idPerson = 184 or idPerson = 183 or idPerson = 182 ; 
+delete from User where idPerson = 190; 
+
+select * from User; 
+
+delete from User where idPerson = 188; 
+
+select * from User; 
 
 
---ATTENTION: do not try again without executing gatilho3_remove.sql
